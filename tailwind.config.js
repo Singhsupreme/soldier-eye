f93f46fa76ae8302b667ae0e5251334de922804d
Singhsupreme <147458552+Singhsupreme@users.noreply.github.com>
2026-05-20 23:15:53 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
         dm: ['DM Sans', 'sans-serif'],
       },
       colors: {
-        bg: '#0C0F13', surface: '#141820', surface2: '#1C2230', surface3: '#232C3E',
-        border: '#2A3347', accent: '#E8B84B', accent2: '#3B82F6',
+        bg: '#0A0D14', surface: '#141820', surface2: '#1C2230', surface3: '#232C3E',
+        border: '#252D45', accent: '#E02B2B', accent2: '#3B82F6',
         danger: '#E24B4A', success: '#1D9E75', warn: '#EF9F27',
       }
     }
